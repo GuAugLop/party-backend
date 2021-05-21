9235295018bf7df2edff41cfb5de1207f6228dff
Gustavo Lopes <gulopes.augusto@gmail.com>
2021-05-20 23:02:05 -0300

    set port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 require("./dbconfig/index");
 
 const cors = require("cors");
